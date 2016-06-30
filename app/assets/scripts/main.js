52ee37f1d93c45e0eb81687134f127305ce49890
Dylan <Dylan@developmentseed.org>
2016-06-29 23:20:53 -0400

    Initial Push. Contains most HTML code, beginning styling

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -6,3 +6,36 @@ OAM.hello();
 
 console.log.apply(console, config.consoleMessage);
 console.log('Environment', config.environment);
+
+// Header Interactivity
+document.querySelector('[data-hook="global-menu:trigger"]').addEventListener('click', function (e) {
+  e.preventDefault();
+  e.target.classList.toggle('button--active');
+
+  document.querySelector('[data-hook="global-menu:wrapper"]').classList.toggle('menu-wrapper--open');
+}, false);
+
+// Grab most recent imagery for the hero image container, if we choose to do that.
+function updateHero () {
+}
+
+// Update the containers for the most recent imagery
+function updateLatest () {
+}
+
+// Add Flexslider to Projects Section
+function createSlider () {
+  $('.flex-slider').flexslider({
+    animation: 'slide',
+    directionNav: true,
+    slideshowSpeed: 6000000,
+  });
+  // $('.flex-next').prependTo('.HOT-Nav-Projects');
+  // $('.flex-control-nav').prependTo('.HOT-Nav-Projects');
+  // $('.flex-prev').prependTo('.HOT-Nav-Projects');
+};
+
+updateHero();
+updateLatest();
+
+console.log("waffles!")
