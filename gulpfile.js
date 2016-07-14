af3161052f9300a23de77cca447cc8223cf04ce6
Daniel da Silva <daniel@developmentseed.org>
2016-07-14 17:22:23 +0100

    Fix paths

--- gulpfile.js
+++ gulpfile.js
@@ -201,8 +201,8 @@ gulp.task('styles', function () {
 });
 
 gulp.task('html', ['styles'], function () {
-  return gulp.src('app/*.html')
-    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
+  return gulp.src('app/**/*.html')
+    .pipe($.useref({base: '.', searchPath: ['.tmp', 'app/**/']}))
     .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.css', $.csso()))
     .pipe($.if(/\.(css|js)$/, rev()))
